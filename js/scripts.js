77aaacc79ba0b639f9b45ee2f26412d40a496a8f
Manuela Garcia <manuela.garcia.dev@gmail.com>
2018-01-11 11:45:49 -0800

    add if..else statements.

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+  var animal = prompt("What animal are you itching to learn about today?");
+
+  if (animal === 'puffins') {
+    $('#puffins').show();
+  } else if (animal === 'quetzals') {
+    $('#quetzals').show();
+  } else {
+    $('#sloths').show();
+  }
+
+
+});
